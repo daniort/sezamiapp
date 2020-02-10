0f00b484a4032a34bd430685ab31161c40ebc3b2
daniort <dqniort@gmail.com>
2020-02-10 17:27:42 -0600

    desing home

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,180 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SEZAMI',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF0076a6),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHome extends StatelessWidget {
+  //final pantalla =MediaQuery.of().size.width;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Sezami Digital'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: new Stack(
+        children: <Widget>[
+          Back(),
+          Container(
+            //Banner
+            color: Colors.blueGrey,
+            height: 200.0,
+            width: MediaQuery.of(context).size.width,
+            child: Mybanner(),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: new EdgeInsets.only(top: 195.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment(0.1, 0.0),
+                            colors: [
+                              const Color(0xff00FF70),
+                              const Color(0xFF00e86e),  
+                            ], // whitish to gray
+                            tileMode: TileMode
+                                .clamp, // repeats the gradient over the canvas
+                          ),
+                        ),
+                        child: Text('hola'),
+                        height: 200.0,
+                        width: 180.0,
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment(1.0, 0.0),
+                            colors: [
+                              const Color(0xFFfbf5bd),
+                              const Color(0xFFf5e557)
+                            ], // whitish to gray
+                            tileMode: TileMode
+                                .clamp, // repeats the gradient over the canvas
+                          ),
+                        ),
+                        child: Text('hola'),
+                        height: 200.0,
+                        width: 180.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment(0.8, 0.0),
+                            colors: [
+                              const Color(0xFF00ffbc),
+                              const Color(0xFF00c8aa)
+                            ], // whitish to gray
+                            tileMode: TileMode
+                                .clamp, // repeats the gradient over the canvas
+                          ),
+                        ),
+                        child: Text('hola'),
+                        height: 200.0,
+                        width: 180.0,
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment(0.8, 0.0),
+                            colors: [
+                              const Color(0xFF9e9da0),
+                              const Color(0xFF786281)
+                            ], // whitish to gray
+                            tileMode: TileMode
+                                .clamp, // repeats the gradient over the canvas
+                          ),
+                        ),
+                        child: Text('hola'),
+                        height: 200.0,
+                        width: 180.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Mybanner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Carousel(
+      images: [
+        AssetImage('images/images1.jpg'),
+        AssetImage('images/images2.jpg'),
+        AssetImage('images/images3.jpg'),
+        //AssetImage('images/images4.jpg')
+      ],
+    );
+  }
+}
+
+class Back extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+              bottom: 0.0,
+              child: Container(
+                //color: Color.fromRGBO(254, 211, 0, 0),
+                color: Color(0xFFfed300),
+                height: 60.0,
+                width: MediaQuery.of(context).size.width,
+              )),
+          Positioned(
+              bottom: 0.0,
+              child: Container(
+                color: Color(0xFF0076a6),
+                height: 6.0,
+                width: MediaQuery.of(context).size.width,
+              ))
+        ],
+      ),
     );
   }
 }
