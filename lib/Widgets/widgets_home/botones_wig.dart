40ce9ee4fe9cae0e7d778eef5702f4d12dbbeac2
daniort <dqniort@gmail.com>
2020-02-17 17:13:28 -0600

    botones modales para los botones del home -avance

--- lib/Widgets/widgets_home/botones_wig.dart
+++ lib/Widgets/widgets_home/botones_wig.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sezamiapp/Widgets/widget_directorio/directorio_wid.dart';
 
 class MisBotonesHome extends StatelessWidget {
@@ -17,13 +18,16 @@ class MisBotonesHome extends StatelessWidget {
               padding: EdgeInsets.all((queryData.size.width) * 0.02),
               child: InkWell(
                  onTap: () => {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Directorio(),
-                            ),
-                          ),
-                        },
+                  showModalBottomSheet(
+                      elevation: (queryData.size.height) * 0.8,
+                      backgroundColor: Color.fromRGBO(0,0,0, 0),
+                      //shape: 
+                      context: context,
+                      isScrollControlled:true,
+                      builder: (context) {
+                        return BotonesDirectorio();
+                      }),
+                },
                 child: Container(
                   width: (queryData.size.width) * 0.45,
                   height: (queryData.size.height) * 0.28,
@@ -125,6 +129,17 @@ class MisBotonesHome extends StatelessWidget {
             new Padding(
               padding: EdgeInsets.all((queryData.size.width) * 0.02),
               child: InkWell(
+                onTap: () => {
+                  showModalBottomSheet(
+                      elevation: (queryData.size.height) * 0.8,
+                      //backgroundColor: Colors.blueGrey,
+                        backgroundColor: Color.fromRGBO(0,0,0, 0),
+                      context: context,
+                      isScrollControlled:true,
+                      builder: (context) {
+                        return Botonesservicios();
+                      }),
+                },
                 child: Container(
                   width: (queryData.size.width) * 0.45,
                   height: (queryData.size.height) * 0.28,
@@ -221,6 +236,335 @@ class MisBotonesHome extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+}
+
+class Botonesservicios extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Container(
+       
+      decoration: BoxDecoration(
+       //color: Color.fromRGBO(1, 1,255, 0.1),
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      width: queryData.size.width,
+      height: (queryData.size.height) * 0.35,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new InkWell(
+              child: Container(
+                width: (queryData.size.width) * 0.90,
+                height: (queryData.size.height) * 0.10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  //color: Colors.purple),
+                  color: Color(0xFF20b9c0),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+
+                        //color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.30,
+                      //child: Image(image: AssetImage("images/icons/ico1.png")),
+                      child: Center(
+                        child: Container(
+                          width: (queryData.size.width) * 0.30,
+                          height: (queryData.size.height) * 0.05,
+                          child:
+                              Image(image: AssetImage("images/icons/ico7.png")),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.60,
+                      child: Center(
+                        child: Text(
+                          "Servicios",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new InkWell(
+              child: Container(
+                width: (queryData.size.width) * 0.90,
+                height: (queryData.size.height) * 0.10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  //color: Colors.purple),
+                  color: Color.fromRGBO(248, 219, 101, 1),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+
+                        //color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.30,
+                      //child: Image(image: AssetImage("images/icons/ico1.png")),
+                      child: Center(
+                        child: Container(
+                          width: (queryData.size.width) * 0.30,
+                          height: (queryData.size.height) * 0.05,
+                          child:
+                              Image(image: AssetImage("images/icons/ico6.png")),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.60,
+                      child: Center(
+                        child: Text(
+                          "VISA",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new InkWell(
+              child: Container(
+                width: (queryData.size.width) * 0.90,
+                height: (queryData.size.height) * 0.10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  //color: Colors.purple),
+                  color: Color(0xFF20b9c0),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      width: (queryData.size.width) * 0.30,
+                      //child: Image(image: AssetImage("images/icons/ico1.png")),
+                      child: Center(
+                        child: Container(
+                          width: (queryData.size.width) * 0.30,
+                          height: (queryData.size.height) * 0.05,
+                          child:
+                              Image(image: AssetImage("images/icons/ico5.png")),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.60,
+                      child: Center(
+                        child: Text(
+                          "Localizador de...",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BotonesDirectorio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Container(
+      decoration: BoxDecoration(
+        //color: Color(0xFFffffff),
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      width: queryData.size.width,
+      height: (queryData.size.height) * 0.35,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new InkWell(
+              child: Container(
+                width: (queryData.size.width) * 0.90,
+                height: (queryData.size.height) * 0.10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  //color: Colors.purple),
+                  color: Color.fromRGBO(0, 228, 115, 1),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+
+                        //color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.30,
+                      //child: Image(image: AssetImage("images/icons/ico1.png")),
+                      child: Center(
+                        child: Container(
+                          width: (queryData.size.width) * 0.30,
+                          height: (queryData.size.height) * 0.05,
+                          child:
+                              Image(image: AssetImage("images/icons/ico7.png")),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.60,
+                      child: Center(
+                        child: Text(
+                          "SEZAMI",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new InkWell(
+              child: Container(
+                width: (queryData.size.width) * 0.90,
+                height: (queryData.size.height) * 0.10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  //color: Colors.purple),
+                  color: Color.fromRGBO(0, 184, 168, 1),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+
+                        //color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.30,
+                      //child: Image(image: AssetImage("images/icons/ico1.png")),
+                      child: Center(
+                        child: Container(
+                          width: (queryData.size.width) * 0.30,
+                          height: (queryData.size.height) * 0.05,
+                          child:
+                              Image(image: AssetImage("images/icons/ico6.png")),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.60,
+                      child: Center(
+                        child: Text(
+                          "Federaciones",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new InkWell(
+              child: Container(
+                width: (queryData.size.width) * 0.90,
+                height: (queryData.size.height) * 0.10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  //color: Colors.purple),
+                  color: Color.fromRGBO(0, 228, 115, 1),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+
+                        //color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.30,
+                      //child: Image(image: AssetImage("images/icons/ico1.png")),
+                      child: Center(
+                        child: Container(
+                          width: (queryData.size.width) * 0.30,
+                          height: (queryData.size.height) * 0.05,
+                          child:
+                              Image(image: AssetImage("images/icons/ico5.png")),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // color: Colors.pink,
+                      ),
+                      width: (queryData.size.width) * 0.60,
+                      child: Center(
+                        child: Text(
+                          "Representaciones",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
