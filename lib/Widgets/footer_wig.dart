5eeff76379d3f0da1afb6913bc217763ade87a46
pelaezH <43900385+pelaezH@users.noreply.github.com>
2020-02-17 16:39:35 -0600

    funcionalidad del futter

--- lib/Widgets/footer_wig.dart
+++ lib/Widgets/footer_wig.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Footer extends StatelessWidget {
+  void customLaunch(command) async {
+    if (await canLaunch(command)) {
+      await launch(command);
+    } else {
+      print(' could not launch $command');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData;
@@ -9,38 +18,44 @@ class Footer extends StatelessWidget {
     var _altura = (queryData.size.height);
     return new Container(
       color: Color(0xFFfed300),
-       child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new IconButton(
-                color: Colors.white,
-                icon: new Icon(FontAwesomeIcons.globe),
-                onPressed: () {/* Your code */},
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: VerticalDivider(
-                  color: Colors.white,
-                ),
-              ),
-              new IconButton(
-                color: Colors.white,
-                icon: new Icon(FontAwesomeIcons.facebookF),
-                onPressed: () {/* Your code */},
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: VerticalDivider(
-                  color: Colors.white,
-                ),
-              ),
-              new IconButton(
-                color: Colors.white,
-                icon: new Icon(FontAwesomeIcons.twitter),
-                onPressed: () {/* Your code */},
-              ),
-            ],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new IconButton(
+            color: Colors.white,
+            icon: new Icon(FontAwesomeIcons.globe),
+            onPressed: () {
+              customLaunch('http://sezami.zacatecas.gob.mx/');
+            },
           ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: VerticalDivider(
+              color: Colors.white,
+            ),
+          ),
+          new IconButton(
+            color: Colors.white,
+            icon: new Icon(FontAwesomeIcons.facebookF),
+            onPressed: () {
+customLaunch('http://www.facebook.com/sezami');
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: VerticalDivider(
+              color: Colors.white,
+            ),
+          ),
+          new IconButton(
+            color: Colors.white,
+            icon: new Icon(FontAwesomeIcons.twitter),
+            onPressed: () {
+customLaunch('http://www.twitter.com/sezami');
+            },
+          ),
+        ],
+      ),
     );
   }
 }
