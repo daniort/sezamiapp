f9445fe48da16e653cd0b41345842f9ac48b15bd
pelaezH <43900385+pelaezH@users.noreply.github.com>
2020-02-18 21:02:49 -0600

    carpeta programas

--- lib/Widgets/widget_directorio/SEZAMI_wid.dart
+++ lib/Widgets/widget_directorio/SEZAMI_wid.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sezamiapp/Widgets/footer_wig.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Sezami extends StatelessWidget {
