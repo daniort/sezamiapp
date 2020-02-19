ff628ec721584161ea1495fe1fb7854c83060e6e
daniort <dqniort@gmail.com>
2020-02-19 17:52:25 -0600

    laa

--- lib/Widgets/widget_directorio/SEZAMI_wid.dart
+++ lib/Widgets/widget_directorio/SEZAMI_wid.dart
@@ -1,5 +1,4 @@
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 import 'package:sezamiapp/models/sezami_model.dart';
 
@@ -18,27 +17,7 @@ class _SezamiState extends State<Sezami> {
       appBar: AppBar(
         title: Text('Sezami'),
       ),
-      body: StreamBuilder(
-        stream:
-            Firestore.instance.collection('{directorio_sezami}').snapshots(),
-        builder:
-            ((BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return CircularProgressIndicator();
-          return ListView.builder(
-              itemCount: snapshot.data.documents.length,
-              itemBuilder: (BuildContext context, int index) {
-                return new Card(
-                  child: new Column(
-                    children: <Widget>[
-                      //new Text(snapshot.data.documents[index].title),
-                      //new Text(snapshot.data.documents[index].content)
-                      new Text('data')
-                    ],
-                  ),
-                );
-              });
-        }),
-      ),
+      body: Text('a')
     );
   }
 }
