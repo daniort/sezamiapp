b883af5df7ad8562188d9593ba6bb01938a22560
daniort <dqniort@gmail.com>
2020-02-21 00:28:44 -0600

    cambien appbar de sezami

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SEZAMI',
+      title: 'SEZAMI Digitalllll',
       theme: ThemeData(
         primaryColor: Color(0xFF0076a6),
       ),
