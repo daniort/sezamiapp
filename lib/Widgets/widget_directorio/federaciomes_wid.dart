2633ce0e485f8c5df429441ed9dde0c48682bf9a
daniort <dqniort@gmail.com>
2020-02-22 04:04:50 -0600

    agrego interacion de los botones de federacion y representacion (aun sin contenido)

--- lib/Widgets/widget_directorio/federaciomes_wid.dart
+++ lib/Widgets/widget_directorio/federaciomes_wid.dart
@@ -1,75 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:sezamiapp/Widgets/footer_wig.dart';
 
 class Federaciones extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'My app title',
-        home: Scaffold(
-            appBar: AppBar(title: Text('Federaciones')),
-            body: Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    ExpansionTile(
-                      title: Text(
-                        'Atlanta, GA.',
-                      ),
-                      children: <Widget>[
-                        ListTile(
-                          title: Text('Laura Vásquez'),
-                        ),
-                         ListTile(
-                          leading: Icon(Icons.people),
-                          title: Text('Presidenta de Bla Bla Bla y Asociados'),
-                        ),
-                         ListTile(
-                          leading: Icon(Icons.mail),
-                          title: Text('laura07@hotmail.com'),
-                        ),
-                         ListTile(
-                          leading: Icon(Icons.location_on),
-                          title: Text('12345 Kimberly Dr. MAletta, GA. 30008'),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.phone),
-                          title: Text('001-234-234-5'),
-                        ),
-                       
-                      ],
-                    ),
-                       ExpansionTile(
-                      title: Text(
-                        'Chicago, IL.',
-                      ),
-                      children: <Widget>[
-                        ListTile(
-                          title: Text('Will Smith'),
-                        ),
-                         ListTile(
-                          leading: Icon(Icons.people),
-                          title: Text('Presidenta de Bla Bla Bla y Asociados'),
-                        ),
-                         ListTile(
-                          leading: Icon(Icons.mail),
-                          title: Text('laura07@hotmail.com'),
-                        ),
-                         ListTile(
-                          leading: Icon(Icons.location_on),
-                          title: Text('12345 Kimberly Dr. MAletta, GA. 30008'),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.phone),
-                          title: Text('001-234-234-5'),
-                        ),
-                       
-                      ],
-                    ),
-                      
-                  ],
-                ),
-              ),
-            )));
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Federaciones'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            new Expanded(child: Text('data')),
+            new Container(
+              height: ((MediaQuery.of(context).size.height) * .07),
+              width: MediaQuery.of(context).size.width,
+              child: Footer(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
