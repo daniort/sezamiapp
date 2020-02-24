2a13ee608859af9429cf2a51217f38c968701e55
daniort <dqniort@gmail.com>
2020-02-24 05:30:29 -0600

    widget federaciones, listo

--- lib/Widgets/widget_directorio/federaciomes_wid.dart
+++ lib/Widgets/widget_directorio/federaciomes_wid.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sezamiapp/Widgets/footer_wig.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -65,11 +66,6 @@ class Federaciones extends StatelessWidget {
                               leading: Icon(Icons.people),
                               title: Text(data['puesto']),
                             ),
-                            if (data['direccion'] != null)
-                              ListTile(
-                                leading: Icon(Icons.pin_drop),
-                                title: Text(data['direccion']),
-                              ),
                             if (data['correo'] != null)
                               ListTile(
                                 leading: Icon(Icons.mail),
@@ -90,20 +86,49 @@ class Federaciones extends StatelessWidget {
                                       '?subject=Asunto: ');
                                 },
                               ),
+                            if (data['direccion'] != null)
+                              ListTile(
+                                leading: Icon(Icons.pin_drop),
+                                title: Text(data['direccion']),
+                                onTap: () {
+                                  var dire = data['direccion'];
+                                  var direurl = dire.replaceAll(" ", "+");
+                                  customLaunch(
+                                      'https://www.google.com/maps/search/?api=1&query=$direurl&z=20&q=32.534916,-117.017154');
+                                },
+                              ),
                             if (data['celular'] != null)
                               ListTile(
                                 leading: Icon(Icons.phone_iphone),
                                 title: Text(data['celular']),
-                                onTap: () {
-                                  customLaunch('tel:' + data['celular']);
+                                onTap: () => {
+                                  showModalBottomSheet(
+                                      elevation: (queryData.size.height) * 0.6,
+                                      backgroundColor:
+                                          Color.fromRGBO(0, 0, 0, 0),
+                                      context: context,
+                                      isScrollControlled: true,
+                                      builder: (context) {
+                                        var phone = data['celular'];
+                                        return BotomModal(data: phone);
+                                      }),
                                 },
                               ),
                             if (data['celular1'] != null)
                               ListTile(
                                 leading: Icon(Icons.phone_iphone),
                                 title: Text(data['celular1']),
-                                onTap: () {
-                                  customLaunch('tel:' + data['celular1']);
+                                onTap: () => {
+                                  showModalBottomSheet(
+                                      elevation: (queryData.size.height) * 0.6,
+                                      backgroundColor:
+                                          Color.fromRGBO(0, 0, 0, 0),
+                                      context: context,
+                                      isScrollControlled: true,
+                                      builder: (context) {
+                                        var phone = data['celular1'];
+                                        return BotomModal(data: phone);
+                                      }),
                                 },
                               ),
                             if (data['oficina'] != null)
@@ -129,6 +154,184 @@ class Federaciones extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+
+class BotomModal extends StatelessWidget {
+  void customLaunch(command) async {
+    if (await canLaunch(command)) {
+      await launch(command);
+    } else {
+      print('no se ejecuto $command');
+    }
+  }
+
+  final data;
+  BotomModal({this.data});
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFffffff),
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      width: queryData.size.width,
+      height: (queryData.size.height) * 0.35,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Expanded(
+            child: Container(
+              width: (queryData.size.width) * 0.80,
+              child: Column(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: (queryData.size.height) * 0.08,
+                      child: Center(
+                        child: Text(
+                          'Abrir con...',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: Row(
+                        children: <Widget>[
+                          new Expanded(
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  new Expanded(
+                                    flex: 2,
+                                    child: InkWell(
+                                      onTap: () {
+                                        customLaunch(
+                                            "whatsapp://send?phone=$data");
+                                      },
+                                      child: Center(
+                                        child: Icon(
+                                          FontAwesomeIcons.whatsappSquare,
+                                          size: 60.0,
+                                          color: Colors.green,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  new Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      child: Center(
+                                        child: Text('WhatsApp'),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          new Expanded(
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  new Expanded(
+                                    flex: 2,
+                                    child: InkWell(
+                                      onTap: () {
+                                        customLaunch("tel:$data");
+                                      },
+                                      child: Center(
+                                        child: Icon(
+                                          FontAwesomeIcons.phoneSquare,
+                                          size: 60.0,
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  new Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      child: Center(
+                                        child: Text('Teléfono'),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          new Expanded(
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  new Expanded(
+                                    flex: 2,
+                                    child: InkWell(
+                                      onTap: () {
+                                        customLaunch("sms:$data");
+                                      },
+                                      child: Center(
+                                        child: Icon(
+                                          FontAwesomeIcons.sms,
+                                          size: 60.0,
+                                          color: Colors.orange,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  new Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      child: Center(
+                                        child: Text('SMS'),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: (queryData.size.height) * 0.1,
+            child: Center(
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Container(
+                  width: (queryData.size.width) * 0.70,
+                  height: (queryData.size.height) * 0.06,
+                  decoration: BoxDecoration(
+                      color: Color(0x1D605e5f),
+                      borderRadius: BorderRadius.circular(30.0)),
+                  child: Center(
+                    child: Text('Cancelar'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
