7d94827dbb2d49d308525351d71b38e6a085e946
daniort <dqniort@gmail.com>
2020-03-02 23:01:54 -0600

    banner mejorado

--- lib/Widgets/widgets_home/banner_wid.dart
+++ lib/Widgets/widgets_home/banner_wid.dart
@@ -4,21 +4,135 @@ import 'package:flutter/material.dart';
 class MiBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Mybanner(),
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return new Carousel(
+      dotColor: Colors.white,
+      dotBgColor: Colors.grey.withOpacity(0.5),
+      images: [
+        InkWell(
+          onTap: () => {
+            showModalBottomSheet(
+                elevation: (queryData.size.height),
+                //backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+                backgroundColor: Colors.white.withOpacity(0.0),
+                context: context,
+                isScrollControlled: true,
+
+                builder: (context) {
+                  //eturn Calendario();
+                  return Img1();
+                }),
+          },
+          child: Container(
+            child: Image.asset('images/banner/apoBanner.jpg'),
+          ),
+        ),
+        InkWell(
+          onTap: () => {
+            showModalBottomSheet(
+                elevation: (queryData.size.height),
+                //backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+                backgroundColor: Colors.white.withOpacity(0.0),
+
+                //shape:
+                context: context,
+                isScrollControlled: true,
+                builder: (context) {
+                  //eturn Calendario();
+                  return Img2();
+                }),
+          },
+          child: Container(
+            child: Image.asset('images/banner/capmBanner.jpg'),
+          ),
+        ),
+        InkWell(
+          onTap: () => {
+            showModalBottomSheet(
+                elevation: (queryData.size.height),
+                //backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+                backgroundColor: Colors.white.withOpacity(0.0),
+  
+                //shape:
+                context: context,
+                isScrollControlled: true,
+                builder: (context) {
+                  //eturn Calendario();
+                  return Img3();
+                }),
+          },
+          child: Container(
+            child: Image.asset('images/banner/coeeBanner.jpg'),
+          ),
+        ),
+      ],
     );
   }
 }
 
-class Mybanner extends StatelessWidget {
+class Img1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Carousel(
-      images: [
-        AssetImage('images/banner/banner1.jpg'),
-        AssetImage('images/banner/banner2.jpg'),
-        AssetImage('images/banner/banner3.jpg'),
-      ],
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        width: queryData.size.width,
+        height: (queryData.size.height)*.8,
+        child: Center(
+          child: Image.asset('images/banner/apoSrc.jpg', width: (queryData.size.width)*.85),
+        ),
+      ),
+    );
+  }
+}
+
+class Img2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Container(
+        decoration: BoxDecoration(
+         color: Colors.white,
+         
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        width: queryData.size.width,
+        height: (queryData.size.height)*.8,
+        child: Center(
+          child: Image.asset('images/banner/capmSrc.jpg', ),
+        ),
+      ),
+    );
+  }
+}
+class Img3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        width: queryData.size.width,
+        height: (queryData.size.height)*.8,
+        child: Center(
+          child: Image.asset('images/banner/coeeSrc.jpg', ),
+        ),
+      ),
     );
   }
 }
