f2833540872b9e3f589140efd18a130504ef0e5a
Pelaez <juanjosepelaez84@gmail.com>
2020-03-10 01:09:13 -0600

    calendario casi queda

--- lib/Widgets/calendario/pruebas.dart
+++ lib/Widgets/calendario/pruebas.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:sezamiapp/Widgets/calendario/eventos_wid.dart';
+import 'package:sezamiapp/Widgets/footer_wig.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 // Example holidays
@@ -15,12 +17,9 @@ void main() {
 class Hola extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Table Calendar Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Table Calendar Demo'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: MyHomePage(),
     );
   }
 }
@@ -43,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    final _selectedDay = DateTime.now();
+    final _selectedDay = DateTime(2021);
 
     _events = {
       _selectedDay.subtract(Duration(days: 1)): [
@@ -51,15 +50,15 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         'Event B0',
         'Event C0'
       ],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 3)): [
+      _selectedDay.subtract(Duration(days: 280)): ['Event A1'],
+      _selectedDay.subtract(Duration(days: 340)): [
         'Event A2',
         'Event B2',
         'Event C2',
         'Event D2'
       ],
-      _selectedDay.subtract(Duration(days: 5)): ['Event A3', 'Event B3'],
-      _selectedDay.subtract(Duration(days: 10)): [
+      _selectedDay.subtract(Duration(days: 325)): ['Event A3', 'Event B3'],
+      _selectedDay.subtract(Duration(days: 310)): [
         'Event A4',
         'Event B4',
         'Event C4'
@@ -72,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 400),
+      duration: const Duration(days: 1),
     );
 
     _animationController.forward();
@@ -99,22 +98,16 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          // Switch out 2 lines below to play with TableCalendar's settings
-          //-----------------------
-          _buildTableCalendar(),
-          // _buildTableCalendarWithBuilders(),
-          const SizedBox(height: 8.0),
-          const SizedBox(height: 8.0),
-          Expanded(child: _buildEventList()),
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(child: _buildTableCalendar()),
+        Expanded(child: new Container(child: _buildEventList())),
+        Expanded(child: Evento()),
+        Container(
+          child: Footer(),
+        )
+      ],
     );
   }
 
@@ -169,30 +162,30 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     );
   }
 
-  Widget _buildHolidaysMarker() {
-    return Icon(
-      Icons.add_box,
-      size: 20.0,
-      color: Colors.blueGrey[800],
-    );
-  }
-
   Widget _buildEventList() {
     return ListView(
-      children: _selectedEvents
-          .map((event) => Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0.8),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                child: ListTile(
-                  title: Text(event.toString()),
-                  onTap: () => print('$event tapped!'),
-                ),
-              ))
-          .toList(),
+      children: _selectedEvents.map((event) => Modal(evento: event)).toList(),
+    );
+  }
+}
+
+class Modal extends StatelessWidget {
+  final evento;
+  Modal({this.evento});
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(width: 0.8),
+          borderRadius: BorderRadius.circular(12.0),
+          color: Colors.amber),
+      width: queryData.size.width,
+      height: (queryData.size.height) * 0.1,
+      child: ListTile(
+        title: Text(evento.toString()),
+      ),
     );
   }
 }
