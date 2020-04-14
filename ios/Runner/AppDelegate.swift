1c3fccfca96c84d69be6c87a1f2231c2681dd785
daniort <dqniort@gmail.com>
2020-04-14 02:40:13 -0500

    re-registro firebase

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,10 +6,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(_ application: UIApplication,
+ 
+
+
+    func application(_ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions:
-    [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
     return true
   }
+
 }
